8af8484b3d29a3759ff100d66f80e885a28c48aa
Kadir <kadirincedev@gmail.com>
2020-08-04 21:34:36 +0300

    Initial Commit

---
+++ FirstCourse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstCourse
+//
+//  Created by Kadir on 4.08.2020.
+//  Copyright © 2020 Kadir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
